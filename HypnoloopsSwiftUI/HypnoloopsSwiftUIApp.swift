a0d7873c76d9adc3c2620104b8281058723d6bd5
Olijujuan Green <olijujuan.green@onepeloton.com>
2023-07-30 22:20:22 -0400

    auth view ui

--- HypnoloopsSwiftUI/HypnoloopsSwiftUIApp.swift
+++ HypnoloopsSwiftUI/HypnoloopsSwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct HypnoloopsSwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AuthenticationView()
         }
     }
 }
