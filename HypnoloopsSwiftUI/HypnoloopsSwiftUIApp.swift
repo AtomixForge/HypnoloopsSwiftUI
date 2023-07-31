61bf916ab3224133fc5412b3673a07deaf36f4c5
Olijujuan Green <olijujuan.green@onepeloton.com>
2023-07-30 20:22:00 -0400

    seed swiftui project

---
+++ HypnoloopsSwiftUI/HypnoloopsSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypnoloopsSwiftUIApp.swift
+//  HypnoloopsSwiftUI
+//
+//  Created by Olijujuan Green on 7/30/23.
+//
+
+import SwiftUI
+
+@main
+struct HypnoloopsSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
