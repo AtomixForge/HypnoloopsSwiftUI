d5e79ea895a1f93511ef4d3290b62351bc9a7ba6
Olijujuan Green <olijujuan.green@onepeloton.com>
2023-08-06 10:46:38 -0400

    user data model

--- HypnoloopsSwiftUI/Models/UserData.swift
+++ HypnoloopsSwiftUI/Models/UserData.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+struct UserData {
+    var username: String
+    var likedAffirmationIds: [String] = []
+
+    init(username: String, likedAffirmationIds: [String]) {
+        self.username = username
+        self.likedAffirmationIds = likedAffirmationIds
+    }
+
+//
+//    mutating func addLikedAffirmation(affirmationId: String) {
+//        if !self.likedAffirmationIds.contains(affirmationId) {
+//            //NetworkManager.userData!.likedAffirmationIds.append(affirmationId)
+//            self.likedAffirmationIds.append(affirmationId)
+//            NetworkManager.shared.updateLikedAffirmations(likedAffirmationIds: self.likedAffirmationIds)
+//        }
+//    }
+//
+//    mutating func removeLikedAffirmation(affirmationId: String) {
+//        let liked = self.likedAffirmationIds.filter { $0 != affirmationId }
+//        self.likedAffirmationIds = liked
+//        NetworkManager.shared.updateLikedAffirmations(likedAffirmationIds: self.likedAffirmationIds)
+//    }
+}
